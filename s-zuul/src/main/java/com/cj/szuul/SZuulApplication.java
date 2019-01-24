6ae0493ebcdf019aa299f57aa71a47f626e1fbbb
XDFHTY <a1668281642@gmail.com>
2019-01-24 18:45:54 +0800

    整合高可用的配置中心

--- s-zuul/src/main/java/com/cj/szuul/SZuulApplication.java
+++ s-zuul/src/main/java/com/cj/szuul/SZuulApplication.java
@@ -2,10 +2,9 @@ package com.cj.szuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @EnableZuulProxy  //开启zuul
@@ -16,13 +15,7 @@ public class SZuulApplication {
 		SpringApplication.run(SZuulApplication.class, args);
 	}
 
-//	//继续寻找配置文件
-//	@Bean
-//	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-//		PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
-//		c.setIgnoreUnresolvablePlaceholders(true);
-//		return c;
-//	}
+
 
 }
 
--- s-zuul/src/main/java/com/cj/szuul/controller/ConfigController.java
+++ s-zuul/src/main/java/com/cj/szuul/controller/ConfigController.java
@@ -1,10 +1,12 @@
 package com.cj.szuul.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //从配置中心动态获取配置文件
 public class ConfigController {
 
     @Value("${foo}")
@@ -14,13 +16,11 @@ public class ConfigController {
     private String fooName;
 
 
+
     @RequestMapping(value = "/foo")
     public String foo(){
-        return foo;
+        return "foo="+foo+",fooname="+fooName;
     }
-    @RequestMapping(value = "/fooName")
-    public String fooName(){
-        return fooName;
-    }
+
 
 }
