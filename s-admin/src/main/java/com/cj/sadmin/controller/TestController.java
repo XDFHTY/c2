6c1d609422c87b6dcbe89bbbe61d4930f8cdab6e
a24568744 <2393803535@qq.com>
2020-01-20 17:39:15 +0800

    修改配置

--- s-admin/src/main/java/com/cj/sadmin/controller/TestController.java
+++ s-admin/src/main/java/com/cj/sadmin/controller/TestController.java
@@ -54,6 +54,6 @@ public class TestController {
 
     @GetMapping("/fo")
     public String getFo() {
-        return fo;
+        return fo+" :    "+port;
     }
 }
--- s-gateway/src/main/java/com/cj/sgateway/SGatewayApplication.java
+++ s-gateway/src/main/java/com/cj/sgateway/SGatewayApplication.java
@@ -2,11 +2,7 @@ package com.cj.sgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,36 +13,4 @@ public class SGatewayApplication {
     }
 
 
-    @Bean
-    @Order(-1)
-    public GlobalFilter a() {
-        return (exchange, chain) -> {
-
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                System.out.println("====>>"+-1);
-            }));
-        };
-    }
-
-    @Bean
-    @Order(0)
-    public GlobalFilter b() {
-        return (exchange, chain) -> {
-
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                System.out.println("====>>"+0);
-            }));
-        };
-    }
-
-    @Bean
-    @Order(1)
-    public GlobalFilter c() {
-        return (exchange, chain) -> {
-
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                System.out.println("====>>"+1);
-            }));
-        };
-    }
 }
