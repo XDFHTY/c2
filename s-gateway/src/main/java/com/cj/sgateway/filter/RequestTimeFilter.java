20627b2a79b3ac162c2fbe2d4cc338d2a334431d
a245687 <a1668281642@gmail.com>
2020-01-22 15:38:42 +0800

    修改配置

---
+++ s-gateway/src/main/java/com/cj/sgateway/filter/RequestTimeFilter.java
@@ -0,0 +1,38 @@
+package com.cj.sgateway.filter;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class RequestTimeFilter implements GatewayFilter, Ordered {
+
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+        return chain.filter(exchange).then(
+                Mono.fromRunnable(() -> {
+                    Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                    if (startTime != null) {
+                        log.info(exchange.getRequest().getURI().getRawPath() + "==>> " + (System.currentTimeMillis() - startTime) + "ms");
+                    }
+                })
+        );
+
+    }
+
+    //值越大则优先级越低
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
+
